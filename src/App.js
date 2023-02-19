d7a1f0f52868c12ba42c398ad4de2fca7a41e78e
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-19 13:22:41 +0300

    Componentlere ilişki verildi

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
 import Navbar from "./Component/Navbar";
+import { Route, BrowserRouter, Routes } from "react-router-dom";
+import Home from "./Component/Home";
+import About from "./Component/About";
+import Contact from "./Component/Contact";
 
 
 function App() {
   return (
-    <div className="App">
-      <Navbar/>
 
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Navbar />
+
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+
+      </div>
+    </BrowserRouter>
   );
 }
 
