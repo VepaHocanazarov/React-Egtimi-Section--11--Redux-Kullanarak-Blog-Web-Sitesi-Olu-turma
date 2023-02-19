3b6f6787d61f46aa72298c2e16ae48bb64f40d52
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-19 12:55:12 +0300

    component içerikleri oluşturuldu ve Navbar import edildi

---
+++ src/Component/Contact.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <div>
+            <div className="container">
+                <h4 className="center">Contact</h4>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est consequatur debitis error harum voluptatibus accusantium voluptate laboriosam labore pariatur. Cumque ipsa itaque, autem consequuntur odio aliquam ratione perferendis reprehenderit veniam.</p>
+            </div>
+        </div>
+    )
+}
+
+export default Contact
