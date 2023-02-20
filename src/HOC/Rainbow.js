a155b7ffe4311567f3d9a5ebdf1422080768e5ee
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-20 11:07:21 +0300

    127. Higher Order Component Devam (Başarılı)

--- src/HOC/Rainbow.js
+++ src/HOC/Rainbow.js
@@ -1,9 +1,13 @@
 import React from 'react'
 
-const Rainbow = () => {
-  return (
-    <div>
-      
+const Rainbow = (WrappedComponent) => {
+
+    const colours =["blue","yellow","black","grrenyellow","pink","red"];
+    const randomcolor= colours[Math.floor(Math.random()*6)]  // Math.floor => sayı 4.6 ise 4 yapar veya 5.6 ise 5 yapar
+    const className = randomcolor+'-text';
+  return (props) => (
+    <div className={className}>
+      <WrappedComponent {...props} />
     </div>
   )
 }
