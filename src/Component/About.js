beba3b507c0976f674a8e1e19f70073190950eb4
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-20 11:12:52 +0300

    Ufak bir  degişiklilik

--- src/Component/About.js
+++ src/Component/About.js
@@ -1,17 +1,9 @@
 import React from 'react'
 
-const About = (props) => {
-
-     console.log(props);
-
-     setTimeout(()=>{
-     props.history.push('/about');
-     },3000
-     )
-
+const About = () => {
  
     return (
-        <div>
+        <div className='blue-text'>
             <div className="container">
                 <h4 className="center">About</h4>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est consequatur debitis error harum voluptatibus accusantium voluptate laboriosam labore pariatur. Cumque ipsa itaque, autem consequuntur odio aliquam ratione perferendis reprehenderit veniam.</p>
