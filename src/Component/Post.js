c5b837b0b3fb59b9609881c8054f602829372cc5
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-22 13:05:49 +0300

    138. BlogDetail (Başarısız)

--- src/Component/Post.js
+++ src/Component/Post.js
@@ -1,46 +1,40 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+import { connect } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
 
 class Post extends Component {
-    state = {
-        post:null
-    };
-
-    componentDidMount = () => {
-        // console.log(this.props);
 
 
-         axios.get("https://jsonplaceholder.typicode.com/posts/1")
-         .then(res => {
-             this.setState({
-                 post: res.data
-             });
-             console.log(res)
-
-         });
-      
-    }
-    
     render() {
 
-        const post = this.state.post ? (
+        const post = this.props.post ? (
             <div className="post">
-                <h4 className='center'> {this.state.post.title} </h4>
-                <p> {this.state.post.body} </p>
+                <h4 className='center'> {this.props.post.title} </h4>
+                <p> {this.props.post.body} </p>
             </div>
         ) : (
             <div className="center">
-            Loading Post...
+                Loading Post...
             </div>
         )
         return (
             <div className='container'>
 
                 {post}
-                
+
             </div>
         );
     }
 }
 
-export default Post;
+const mapStateToProps = (state,) => {
+
+    const {productId} = useParams();
+
+    return {
+        post: state.posts.find(post => post.id === productId)
+
+    }
+}
+
+export default connect(mapStateToProps)(Post);
