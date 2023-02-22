cfefbac228372b1e28eb01bfd22b2bbfb8dd9bd6
Vepa Hocanazarov <vefa00055@gmail.com>
2023-02-22 12:16:01 +0300

    137. Componontten Store a Erişme

--- src/Component/Home.js
+++ src/Component/Home.js
@@ -1,29 +1,14 @@
 import React, { Component } from 'react'
 import axios from "axios";
 import { Link } from 'react-router-dom';
-import Pokeboll from "../ball.png"
+import Pokeboll from "../ball.png";
+import { Connect, connect } from 'react-redux';
 
 class Home extends Component {
 
-    state = {
-        posts: []
-    }
-
-    componentDidMount = () => {
-        axios.get("https://jsonplaceholder.typicode.com/posts")
-            .then(res => {
-                // console.log(res);
-                this.setState({
-                    posts: res.data.slice(0, 20)
-                })
-
-            });
-    }
-
-
     render() {
 
-        const { posts } = this.state
+        const { posts } = this.props
         const postList = posts.length ? (
             posts.map(post => {
                 return (
@@ -54,8 +39,12 @@ class Home extends Component {
             </div>
         )
     }
-
-
 }
 
-export default Home
+const Send  = (state) =>{
+    return{
+        posts:state.posts
+    }
+}
+
+export default connect (Send) (Home);
--- src/reducers/rooteReducer.js
+++ src/reducers/rooteReducer.js
@@ -1,6 +1,12 @@
 
 const initState = {
-    posts: []
+    posts: [
+        {id: 1, title: 'sunt aut facere repellat provident occaecati excepturi optio reprehenderit', body: 'quia et suscipit\nsuscipit recusandae consequuntur …strum rerum est autem sunt rem eveniet architecto' },
+
+       {id: 2, title: 'qui est esse', body: 'est rerum tempore vitae\nsequi sint nihil reprehend…aperiam non debitis possimus qui neque nisi nulla' },
+
+       {id: 3, title: 'ea molestias quasi exercitationem repellat qui ipsa sit aut', body: 'et iusto sed quo iure voluptatem occaecati omnis eligendi aut ad voluptatem doloribus vel accusantium quis pariatur molestiae porro eius odio et labore et velit aut'}
+    ]
 }
 
 const rooteReducer = (state = initState, action) => {
